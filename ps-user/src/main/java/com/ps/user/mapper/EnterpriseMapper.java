ae0d811045728cf154fcd1b6a6dc056bba9b9a2f
ryan <nan-yi@qq.com>
2026-03-11 10:26:46 +0800

    feat(enterprise): 实现企业用户管理功能
    - 添加EnterpriseMapper数据访问层
    - 实现EnterpriseService业务接口和实现类
    - 实现EnterpriseController REST控制器
    - 支持企业创建、查询、修改
    - 支持企业启用/禁用
    - 支持企业列表分页查询
    
    Closes #T008

---
+++ ps-user/src/main/java/com/ps/user/mapper/EnterpriseMapper.java
@@ -0,0 +1,9 @@
+package com.ps.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ps.core.entity.Enterprise;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EnterpriseMapper extends BaseMapper<Enterprise> {
+}
