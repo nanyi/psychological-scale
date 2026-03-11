055f30dcee715499b605d4e1e7ccf7efdf961b88
ryan <nan-yi@qq.com>
2026-03-11 10:34:35 +0800

    feat(scale): 实现题目与选项管理功能
    - 实现QuestionMapper数据访问层
    - 实现DimensionMapper数据访问层
    - 实现QuestionService业务接口和实现类
    - 实现QuestionController REST控制器
    - 支持题目创建、修改、删除、排序
    - 支持维度创建、修改、删除
    - 支持量表题目和维度查询
    
    Closes #T011

---
+++ ps-scale/src/main/java/com/ps/scale/mapper/DimensionMapper.java
@@ -0,0 +1,9 @@
+package com.ps.scale.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ps.core.entity.Dimension;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DimensionMapper extends BaseMapper<Dimension> {
+}
