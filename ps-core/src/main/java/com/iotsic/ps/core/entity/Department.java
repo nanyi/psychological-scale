205381ab16aeeb5daf4d0532a4339e5f23fb23ec
ryan <nan-yi@qq.com>
2026-03-18 15:21:12 +0800

    feat: 添加Department实体类（部门表）
    - 新增Department.java实体类
    - 对应数据库表: sys_department

---
+++ ps-core/src/main/java/com/iotsic/ps/core/entity/Department.java
@@ -0,0 +1,62 @@
+package com.iotsic.ps.core.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 部门实体类
+ * 
+ * @author Ryan
+ * @since 2026-03-18
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_department")
+public class Department extends BaseEntity {
+
+    /**
+     * 部门名称
+     */
+    private String departmentName;
+
+    /**
+     * 父部门ID（0=一级部门）
+     */
+    private Long parentId;
+
+    /**
+     * 企业ID
+     */
+    private Long enterpriseId;
+
+    /**
+     * 负责人
+     */
+    private String leader;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 排序
+     */
+    private Integer sortOrder;
+
+    /**
+     * 状态：0-禁用，1-启用
+     */
+    private Integer status;
+
+    /**
+     * 描述
+     */
+    private String description;
+}
