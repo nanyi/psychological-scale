db01e2eb14a276d9bf04d0dfcb29fd69857fd9d7
ryan <nan-yi@qq.com>
2026-04-21 09:31:54 +0800

    refactor: 调整thirdparty模块迁移

--- ps-scale/src/main/java/com/iotsic/ps/scale/thirdparty/mapper/SyncLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.iotsic.ps.scale.thirdparty.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.iotsic.ps.scale.thirdparty.entity.SyncLog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SyncLogMapper extends BaseMapper<SyncLog> {
-}
